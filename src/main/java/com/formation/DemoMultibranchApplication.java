ea1f406e66198f10e521e7df80477c1208d47e30
mm167 <mm167@example.com>
2024-05-19 20:57:46 +0200

    Main v1

---
+++ src/main/java/com/formation/DemoMultibranchApplication.java
@@ -0,0 +1,19 @@
+package com.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoMultibranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMultibranchApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String getGreeting() {
+		return "<h2>Hello world.</h2>";
+	}
+
+}
